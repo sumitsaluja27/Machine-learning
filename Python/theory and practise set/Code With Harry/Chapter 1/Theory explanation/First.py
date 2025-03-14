607e3f6fa9d0d0ad6707deebd7548ab57a7986ae
sumitsaluja27 <sumitsaluja.iiim@gmail.com>
2025-03-14 21:38:15 +0530

    Update First.py
    Updated pyjokes file with comments

--- Python/theory and practise set/Code With Harry/Chapter 1/Theory explanation/First.py
+++ Python/theory and practise set/Code With Harry/Chapter 1/Theory explanation/First.py
@@ -7,5 +7,6 @@ print(joke)
     # So thanks for choosing this file 
     # but this is only my first file
     # I still need to add a lot things
-    # in this
-    
+    # in this I have imported a module which is called as pyjokes and import jokes by putting
+        # when you add # in front of any sentence it becomes a comment.It wont show in output. We are making notes as theory
+        # if we remove # from #print("hi, how are you?") then it will give output of hi, how are you?
